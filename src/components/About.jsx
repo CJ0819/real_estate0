51cac23833abd43aab159a97453b280c58b09b4c
Joseph Conduah <josephconduah02@gmail.com>
2025-09-29 19:02:40 +0000

    final-commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { assets } from '../assets/assets'
 
+
 const About = () => {
   return (
     <div className='flex flex-col items-center justify-center container mx-auto p-14 md:px-20 lg:px-32 w-full overflow-hidden' id="About">
